69456494e69a4a6de9f8a41610605707c62dd8d2
Hiroyuki Sano <sh19910711@gmail.com>
2013-12-01 21:55:52 +0900

    初期化の処理を追加

---
+++ cpu.js
@@ -0,0 +1,28 @@
+// CPU
+var Processor = function(clock_speed) {
+  this.registers = {
+    RA: 0,
+    RB: 0,
+    RC: 0,
+    RD: 0,
+    A: 0,
+    PC: 0,
+    SP: 0,
+    F: 0
+  };
+};
+
+Processor.prototype = {
+  // 初期化を行う
+  init: function() {
+    this.registers.RA = 0;
+    this.registers.RB = 0;
+    this.registers.RC = 0;
+    this.registers.RD = 0;
+    this.registers.A  = 0;
+    this.registers.PC = 0;
+    this.registers.SP = 0;
+    this.registers.F  = 0;
+  }
+};
+
